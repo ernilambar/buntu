be089e14ace63d716840a5fbf5f2014b74aa2134
Nilambar Sharma <nilambar@outlook.com>
2016-01-09 12:02:32 +0545

    change custom header

--- functions.php
+++ functions.php
@@ -11,9 +11,16 @@ add_action( 'wp_enqueue_scripts', 'buntu_enqueue_styles' );
 
 function buntu_theme_setup() {
 
+    unregister_default_headers( array(
+        'horizon',
+        'orange-burn',
+        'planet-burst',
+        'space-splatters',
+    ) );
+
 }
 
-add_action( 'after_setup_theme', 'buntu_theme_setup', 11 );
+add_action( 'after_setup_theme', 'buntu_theme_setup', 99 );
 
 function buntu_color_primary_default( $color ) {
     $color = '230d5b';
@@ -21,3 +28,25 @@ function buntu_color_primary_default( $color ) {
 }
 
 add_filter( 'theme_mod_color_primary', 'buntu_color_primary_default' );
+
+function buntu_custom_header() {
+
+    add_theme_support(
+        'custom-header',
+        array(
+            'default-image'          => '%s/images/headers/planets-blue.jpg',
+            'random-default'         => false,
+            'width'                  => 1175,
+            'height'                 => 400,
+            'flex-width'             => true,
+            'flex-height'            => true,
+            'default-text-color'     => '252525',
+            'header-text'            => true,
+            'uploads'                => true,
+            'wp-head-callback'       => 'stargazer_custom_header_wp_head'
+        )
+    );
+
+}
+
+add_action( 'after_setup_theme', 'buntu_custom_header' );
